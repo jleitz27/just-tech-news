802b4156ef8ced9326e65365a07db37b07452f11
Jason L <jkleitz27@gmail.com>
2022-06-21 12:51:27 -0400

    added hashes instead of password

--- models/User.js
+++ models/User.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -8,32 +10,44 @@ class User extends Model {}
 User.init(
     {
         id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
         },
         username: {
-        type: DataTypes.STRING,
-        allowNull: false
+            type: DataTypes.STRING,
+            allowNull: false
         },
         email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
             isEmail: true
-        }
+            }
         },
         password: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
             len: [4]
+            }
         }
-        }
-    },
-    {
+        },
+        {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -52,7 +52,8 @@ router.put('/:id', (req, res) => {
 
   // pass in req.body instead to only update what's passed through
     User.update(req.body, {
-    where: {
+        individualHooks: true,
+        where: {
         id: req.params.id
         }
     })
