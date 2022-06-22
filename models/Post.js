452f9ea1e49f4d1ba91c76d63068a3aeb4544283
Jason L <jkleitz27@gmail.com>
2022-06-22 13:12:42 -0400

    updating routes with post routes

--- models/Post.js
+++ models/Post.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
 // create our Post model
 class Post extends Model {}
 
@@ -8,28 +7,28 @@ class Post extends Model {}
 Post.init(
     {
         id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
         },
         title: {
-            type: DataTypes.STRING,
-            allowNull: false
+        type: DataTypes.STRING,
+        allowNull: false
         },
         post_url: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
             isURL: true
-            }
+        }
         },
         user_id: {
-            type: DataTypes.INTEGER,
-            references: {
+        type: DataTypes.INTEGER,
+        references: {
             model: 'user',
             key: 'id'
-            }
+        }
         }
     },
     {
