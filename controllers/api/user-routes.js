3b120e68df6a2c4d4a4055cf6fb63f2daae03871
Jason L <jkleitz27@gmail.com>
2022-07-07 16:24:38 -0400

    better working files

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const { User, Post, Comment, Vote } = require('../../models');
-const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
@@ -54,7 +53,7 @@ router.get('/:id', (req, res) => {
     });
 });
 
-router.post('/', withAuth, (req, res) => {
+router.post('/', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
   User.create({
     username: req.body.username,
@@ -76,7 +75,7 @@ router.post('/', withAuth, (req, res) => {
     });
 });
 
-router.post('/login', withAuth, (req, res) => {
+router.post('/login', (req, res) => {
   // expects {email: 'lernantino@gmail.com', password: 'password1234'}
   User.findOne({
     where: {
@@ -116,7 +115,7 @@ router.post('/logout', (req, res) => {
   }
 });
 
-router.put('/:id', withAuth, (req, res) => {
+router.put('/:id', (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
@@ -139,7 +138,7 @@ router.put('/:id', withAuth, (req, res) => {
     });
 });
 
-router.delete('/:id', withAuth, (req, res) => {
+router.delete('/:id', (req, res) => {
   User.destroy({
     where: {
       id: req.params.id
